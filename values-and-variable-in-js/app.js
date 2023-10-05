a6774d035e0f20638e1b7846e8baa4d9154b7da4
rasool bashir <bashirrasool0@gmail.com>
2023-10-05 05:35:54 -0700

    class1

---
+++ values-and-variable-in-js/app.js
@@ -0,0 +1,42 @@
+    // (1) VALUES AND VARIABLES IN JAVASCRIPT
+
+
+        //   (1)
+// var myName = ("Ghulam rasool");
+// console.log(myName);
+
+
+
+
+        //    (2)
+// var $myName =("ghulam rasool");
+// console.log($myName)
+   
+
+
+
+//       (3)
+// var _myAge ="20";
+// console.log (_myAge)
+
+
+
+//       (4)
+// var _my_Age ="25";
+// console.log (_my_Age)
+
+//var IS variable (key)
+//myName IS Name of variable
+// = ("ghulam") IS value
+   
+
+
+   //NAMING VARIABLE : RULES
+   //  (1) The first character must be underscore( _ )or ans ($).you can not use number as the first character
+   //example:-
+//    var 1myName = ("ghulam") it is wrong method
+
+// (2) Variable name are case sensitive
+// (3) no limits of var in js 
+
+
