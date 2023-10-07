281f5a6980e8facd0af1987ce99c9b8fad5dc032
rasool bashir <bashirrasool0@gmail.com>
2023-10-07 06:37:10 -0700

    class 2 work is done

---
+++ data type in js/app.js
@@ -0,0 +1,106 @@
+//    THER ARE 4 MAIN PRIMITIVE DATA TYPE IN JS
+ 
+//    1 = undefine
+//    2 = Boolean
+//    3 = Number
+//    4 = String
+ 
+
+
+// var myName = "rasool bashir";
+// console.log(myName);
+
+// typeof operator
+     // (1) STRING
+// console.log (typeof(myName)); 
+
+      // (2) NUMBERS
+// var myAge = 20;
+// console.log(myAge);
+// console.log(typeof(myAge));
+
+      //(3) BOOLEAN
+    //   var iamRasool = true;
+    //   console.log(iamRasool);
+    //   console.log(typeof(iamRasool));
+  
+         //(4) undefined
+        //  var sports;
+        //  console.log(sports);
+
+
+        //  difference b\w null and undefined
+               // null
+        // var iamRasool = null;
+        // console.log(iamRasool); 
+        // console.log(typeof(iamRasool)) // it is 2nd bug in js 
+             // undefine
+        // var sports;
+        // console.log(sports);
+        // console.log(typeof(sports))
+
+
+         //challenge
+         // x 10 +"20"
+         // x 9 - "5"
+         // x "java" + "script"
+         // x " " + " "
+         // x "rasool" - "bashir"
+         // x true + true
+        // x true + false
+         // x false + true
+         // x false - true
+
+// console.log(10 +"20")
+// console.log(9 - "5") // bug
+// console.log("java" + "script")
+// console.log(" " + " ")
+// console.log("rasool" - "bashir")
+// console.log(true + true) // inthis 1 represent true and 0 represent false
+// console.log(true + false)
+// console.log(false + true)
+// console.log(false - true)
+
+// question (Q) what is NaN?
+// WHEN WE SUBTRACT TWO String IT GIVE NaN
+// IT IS THE PROPERITY OF GLOBAL OBJECT
+// IT OTHER WORD, IT IS AVAILABE IN GLOBAL SCOPE 
+// EXAMPLE NO 1:-
+// console.log("rasool" - "bashir")
+
+// EXAMPLE NO 2:-
+// var myAge = 20;
+// var myName ="RASOOL BASHIR"
+// console.log(isNaN(myAge));
+// console.log(isNaN(myName));
+ 
+
+// challange
+//   x NaN === NaN;
+//   X Number.NaN === NaN;
+//   x isNaN (NaN);
+//   isNaN(Number.NaN);
+//   Number.isNaN(NaN);
+
+
+console.log(NaN === NaN);
+console.log(Number.NaN === NaN);
+console.log(isNaN (NaN));
+console.log(isNaN(Number.NaN));
+console.log(Number.isNaN(NaN));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
