3d3c3ce0a0cd1c28dd1fb0606175fed7086db9ee
rasool bashir <bashirrasool0@gmail.com>
2023-10-08 11:58:19 -0700

    class 3 work is not complete i am working on it

---
+++ expressions and operators/app.js
@@ -0,0 +1,61 @@
+   // EXPRESSIONS AND OPERATORS
+   // 1 = assignment operators
+   // 2 = arithmetic operators
+   // 3 = copmarsion operators
+   // 4 = logical operators
+   // 5 = string operators
+   // 6 = conditional(ternary) operators
+
+
+         // (1)ASSIGNMENT OPERATOR
+ //an assignment operator assigns a value to its left operand
+ //based on the value of its right operand
+ //The simple asignment operator is equal (=)
+//Example:-
+// var x = 5;
+// var y = 6;
+
+
+//  var x = 6;
+//  var y = 6;
+// //  console.log("is both x and y are equal or not" + x==y); this method give false value because here we use + operator (+ operator is use to concunation)
+// console.log (`Is both the x and y are equal : ${x ==y}`); it is ecmascript method it will give you true answer
+                       // ARITHIMETIC OPERATOR
+// an arithimetic operator takes numerical values
+//(either literals or variables)as their operandsand
+//returns a single numerical vALUES.
+    //    console.log(3+3);
+    //    console.log(10-5);
+    //    console.log(20/5);
+    //    console.log(5*6);
+    //    console.log("reminder operator " + 81%7);
+       // incerment and decrement operator
+// operator : x++ or ++x
+// if used postfix ,with operator after operand (for example:, x++),
+// the incriment operator increments and returns the value before incrementing
+    // (1)
+// var num = 15;
+// var newNum = num++ + 5;
+// console.log(num);
+// console.log(newNum);
+       // (2)
+// var num = 15;
+// var newNum = num-- + 5;
+// console.log(num);
+// console.log(newNum);
+
+// if used prefix ,with operator after operand (for example:, ++x),
+// the incriment operator increments and returns the value after incrementing
+       // (1)
+// var num = 15;
+// var newNum = ++num + 5;
+// console.log(num);
+// console.log(newNum);
+
+         // (2)
+var num = 15;
+var newNum = --num + 5;
+console.log(num);
+console.log(newNum);
+
+
