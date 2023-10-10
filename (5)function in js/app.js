ef0489c6c0d6198697cd22f013af6f90b3ca12d6
rasool bashir <bashirrasool0@gmail.com>
2023-10-10 12:07:40 -0700

    ading new work of class 5

---
+++ (5)function in js/app.js
@@ -0,0 +1,105 @@
+            //FUNCTION IN JS
+// FUNCTION DEFINATION
+// CALLING FUNCTION
+// FUNCTION PARAMETER
+// FUNCTION ARGUMENTS
+// FUNCTION EXPRESSION
+// RETURN KEYWORD
+// ANONYMOUS FUNCTION
+
+
+                           //(*1*)FUNCTION DEFINATION
+// A FUNCTION DEFINITION (ALSO CALLED DECLARATION OR FUNCTION STATEMENT)COSIST OF THE FUNCTION KEYWORD,FOLLOWED BY,
+
+//THE NAME OF FUNCTION.
+//A LIST OF PARAMETER TO FUNCTION,ENCLOSED IN PARENTHESS AND SEPARATED BY COMMAS.
+//THE JS STATEMENT THAT DEFINE THE FUNCTION,ENCLOSED IN CURLY BRACKETS,{...}.
+
+//SYNTAX OF FUNCTION;-
+//    function functionName()
+//  {
+    //statement
+//  }
+
+
+// who to add Number in function
+
+// it is simple method not function method
+// var a = 10;
+// var b = 20;
+// var sum = a+b;
+// console.log(sum);
+  
+// it is function method
+// function sum(){
+// var a = 10, b = 15;
+// var total = a+b;
+// console.log(total);
+// }
+                          //(*2)CALLING FUNCTION
+
+
+                        //   function sum(){
+                        //     var a = 10, b = 5;
+                        //     var total = a+b;
+                        //     console.log(total);
+                        //     }
+                        //     sum();
+
+
+
+           //(*3*)FUNCTION PARAMETER VS FUNCTION ARGUMENT
+// FUNCTION PARAMETER ARE THE NAMES LISTED IN THE FUNCTION 'S DEFINATION
+// FUNCTION ARGUMENTS ARE THE REAL VALUES PASSED TO THE FUNCTION.
+
+// in this the parameter is (a,b)
+// function sum(a,b){
+// // var a = 10, b = 15; it this type of function we don't use this
+
+// var total = a+b;
+// console.log(total);
+// }
+// sum();
+// // in this argument is (20,30)
+// sum(20,30); 
+// sum(10,20);
+// sum(6,6);
+
+               //(*4*)FUNCTION EXPRESSION
+// IT CREATE A FUNCTION AND PUT IT INTO THE VARIABLE               
+// function sum(a,b){
+// var total = a+b;
+// console.log(total);
+// }
+// var functionExpression = sum(5,15);
+
+// sum(5,10);
+
+
+
+                        //(*5*)return keywor
+// jub hamara function return statement dakha ga to hamara function wahi stop ho ga or auska executing khatam ho jyga
+//     function sum(a,b){
+//     return total = a+b;
+//     }
+//     var functionExpression = sum(15,15);
+// console.log(`the sum of two no is ` + functionExpression);
+
+
+
+             //(*6*)ANONYMOUS function
+
+             var functionExpression =function(a,b){
+                return total = a+b;
+                }
+                var sum = functionExpression(5,5) ;
+            console.log('the sum of two no is ' + sum);
+
+
+
+
+
+
+
+
+
